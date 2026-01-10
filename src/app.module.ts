a52c785c0206d88c319d782b13126a90e1a94d07
root <root@srv1210791.hstgr.cloud>
2026-01-10 09:44:13 +0000

    stable push

--- src/app.module.ts
+++ src/app.module.ts
@@ -32,6 +32,7 @@ import { Comment } from "./modules/comments/entities/comment.entity";
       redis: {
         host: "127.0.0.1",
         port: 6379,
+	keyPrefix: "stable",
         maxRetriesPerRequest: null,
         enableReadyCheck: false,
       },
